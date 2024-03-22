268145a968a9a4ccec730f7b932e01cbbc8b6071
Furkan Çakıcı <furkancakici25@gmail.com>
2024-03-22 22:07:51 +0300

    refactor: Update footer and header styles

--- src/components/organism/footer.tsx
+++ src/components/organism/footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-    return <footer>This is Footer</footer>
+    return (
+        <footer className='text-center bottom-0 z-50 w-full border-t border-border/60 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+            By Furkan Çakıcı
+        </footer>
+    )
 }
 
 export default Footer
--- src/components/organism/header.tsx
+++ src/components/organism/header.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from '@/components/molecules/mode-toggle'
 
 const Header = () => {
     return (
-        <header className='sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+        <header className='sticky top-0 z-50 w-full border-b border-border/60 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
             <div className='container flex h-14 max-w-screen-2xl items-center'>
                 <div className='flex mr-4 justify-center items-center'>
                     <div className='flex items-center mr-6 space-x-2'>
