53b2a02bc34d0d777ddea668785d8e9d9ae16db7
Furkan Çakıcı <furkancakici25@gmail.com>
2024-03-22 18:20:28 +0300

    feat: added main-layout

--- src/pages/main-layout.tsx
+++ src/pages/main-layout.tsx
@@ -1,7 +1,21 @@
-import { ReactNode } from 'react'
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
 
-const MainLayout = ({ children }: { children: ReactNode }) => {
-    return <div className='relative flex min-h-screen flex-col bg-background'>{children}</div>
+import Footer from '@/components/organism/footer'
+import Header from '@/components/organism/header'
+
+const MainLayout = () => {
+    return (
+        <div className='relative flex flex-col min-h-screen bg-background'>
+            <Header />
+            <div className='flex-1'>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Outlet />
+                </Suspense>
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default MainLayout
