31cf9c8d2be2ca903537a6204e2cb8c206c7cb11
Furkan Çakıcı <furkancakici25@gmail.com>
2024-03-26 05:25:33 +0300

    chore: change some i18n translations config

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next'
 import detector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-import en from '@public/locales/en/en.json'
-import tr from '@public/locales/tr/tr.json'
+import en from './en.json'
+import tr from './tr.json'
 
 i18n.use(detector)
     .use(initReactI18next)
