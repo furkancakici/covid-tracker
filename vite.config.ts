7d9f6bbffad13919b740b764caa7ad537608209e
Furkan Çakıcı <furkancakici25@gmail.com>
2024-03-26 04:12:30 +0300

    feat: add Dockerfile and .dockerignore files

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: true,
+        strictPort: true,
+        port: 3000
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
