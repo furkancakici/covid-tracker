765ceab4a02d64d3da5468105af5db950a1701fc
Furkan Çakıcı <furkancakici25@gmail.com>
2024-03-26 07:15:43 +0300

    refactor: fix flag URL in useCountryInfo hook

--- src/hooks/use-country-info.tsx
+++ src/hooks/use-country-info.tsx
@@ -12,7 +12,7 @@ function useCountryInfo(isoCode: string) {
             const countryCode = isoCode ?? 'USA'
             const iso2 = alpha3ToAlpha2(countryCode)
             const name = alpha3ToCountryName(countryCode)
-            const flag = `https://flagsapi.com/${iso2.toUpperCase()}/flat/32.png`
+            const flag = `https://flagsapi.com/${iso2!.toUpperCase()}/flat/32.png`
 
             setCountryName(name!)
             setCountryIso2(iso2!)
